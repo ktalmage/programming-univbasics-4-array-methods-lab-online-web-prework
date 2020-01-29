5fcfc780e09d25e2c6dcc76f375153b7281ea105
Kyle Talmage <ktalmage85@gmail.com>
2020-01-29 21:26:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,18 +11,20 @@ end
   puts using_sort(array)
 def using_reverse(array)
   array.reverse
-
 end
   puts using_reverse(array)
 
 def using_first(array)
-
+  array.first
 end
+  puts using_first(array)
 
 def using_last(array)
-
+  array.last
 end
+  puts using_last(array)
 
 def using_size(array)
-
+  array.size
 end
+  puts using_size
